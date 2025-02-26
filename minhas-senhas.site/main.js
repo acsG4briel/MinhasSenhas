a1d03241ebbd331dd81310afd039ad19b3dc7e74
Gabriel Silva <gabriel.araujocsilv@gmail.com>
2025-02-26 17:54:32 -0300

    FEAT #0001 - Configuração Inicial Electron

---
+++ minhas-senhas.site/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(`file://${path.join(__dirname, 'build/index.html')}`);
+  mainWindow.webContents.openDevTools();
+});
